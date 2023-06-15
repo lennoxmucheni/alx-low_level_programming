903c95e32cc8513004e1cd000706545bb5edc931
Lennox Mucheni <lennoxmucheni@gmail.com>
2023-06-16 01:54:59 +0300

    C Program that prints specified string(s)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	puts(Mystr)
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
