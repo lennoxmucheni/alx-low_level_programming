83434994a4128f6234f6ab44c725a6d52d40fe5e
root <lennoxmucheni@gmail.com>
2023-06-16 03:49:30 +0300

    C Program that prints specified string(s) using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-/* Using puts in place of printf */
+/**
+* main - Entry point
+* Description: 'Using puts in place of printf to display a string of characters'
+* @parameter: puts
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
