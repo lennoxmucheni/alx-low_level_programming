2494a27c87dd7b2c3df3c460b6e13db0ee664d60
root <lennoxmucheni@gmail.com>
2023-06-17 14:10:20 +0300

    print the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'Display positive & negative nos.'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
