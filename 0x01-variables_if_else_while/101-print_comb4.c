e68120197be601da033ddca33e5ef39ef448e1b8
root <lennoxmucheni@gmail.com>
2023-06-17 15:08:35 +0300

    Print all possible combinations of two digit nos

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'Print all possible combinations of two digit nos'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int firstDigit;
+	int secondDigit;
+	int thirdDigit;
+
+	for (firstDigit = 0; firstDigit < 9; firstDigit++)
+	{
+		for (secondDigit = firstDigit + 1; secondDigit <= 9; secondDigit++)
+		{
+			for(thirdDigit = secondDigit + 1; thirdDigit <=9; thirdDigit++)
+			{
+				putchar(firstDigit + '0');
+				putchar(secondDigit + '0');
+				putchar(thirdDigit + '0');
+
+				if (firstDigit != 7 || secondDigit != 8 || thirdDigit != 9)
+			
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
