05c72a8f00346339676a7be2f08368b01760f602
root <lennoxmucheni@gmail.com>
2023-06-17 13:47:49 +0300

    print all single digit numbers of base 10 using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int number;
 
-	for (number = '0'; number < '10'; number++)
+	for (number = 0; number < 10; number++)
 	{
-		putchar(number);
+		putchar('number');
 	}
 	putchar('\n');
 	return (0);
