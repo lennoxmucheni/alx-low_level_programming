0adc906ad761ebec2c3b55201d48e8c7ff1e7911
root <lennoxmucheni@gmail.com>
2023-06-17 11:20:14 +0300

    If ... else if ... else statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 	printf("Last digit of %d is ", n);
 
 	lastDigit = abs(n % 10);
-	if (lastDigit > 5) 
+	if (lastDigit > 5)
 	{
 	printf("%d and is greater than 5\n", lastDigit);
-	} 
-	else if (lastDigit == 0) 
+	}
+	else if (lastDigit == 0)
 	{
 	printf("%d and is 0\n", lastDigit);
 	}
-	else 
+	else
 	{
 	printf("%d and is less than 6 and not 0\n", lastDigit);
 	}
