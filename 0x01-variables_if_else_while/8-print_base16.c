a93cdc48e0bc36adb3630516f2efc9a2378bf785
root <lennoxmucheni@gmail.com>
2023-06-17 14:24:12 +0300

    print all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'Print alphabets in reverse'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char base16;
+	
+	for (base16 = '0'; base16 <= 9; base16++)
+	{
+		putchar(base16);
+	}
+	for (base16 = 'a'; base16 <= 'f'; base16++)
+	{
+		putchar(base16);
+	}
+	putchar('\n');
+	return (0);
+}
