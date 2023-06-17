59b0d0608396e9abf9f9d93dead6affbcc4fc93e
root <lennoxmucheni@gmail.com>
2023-06-17 12:09:07 +0300

    Print the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'Display positive & negative nos.'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char = letter;
+	for (letter = 'a'; letter =< z; letter++)
+	{
+		putchar(letter);
+	}
+	putchar(\n);
+	return (0);
+}
