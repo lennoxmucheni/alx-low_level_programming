8895b73e62e29b4fabfdd391b966bbb91d1d1460
root <lennoxmucheni@gmail.com>
2023-06-17 14:45:05 +0300

    print all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,18 +4,18 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number < 10; number++) 
+	for (number = 0; number < 10; number++)
 	{
 	putchar(number + '0');
 
-	if (number != 9) 
+	if (number != 9)
 		{
 		putchar(',');
 		putchar(' ');
 		}
 	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
