2223f1ee0da9914fc2904d429a8f97430a30e2ca
root <lennoxmucheni@gmail.com>
2023-06-17 13:19:08 +0300

    Print single digit nos of base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int number;
 
-	for (number = '0'; number <= '9'; number++)
+	for (number = '0'; number < '10'; number++)
 	{
-		printf('%d', number);
+		printf("%d", number);
 	}
 	printf("\n");
 	return (0);
