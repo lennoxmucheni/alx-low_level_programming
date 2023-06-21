aa6831b7f99ed3ceda985ab58da7972aa2d230fe
Lennox Mucheni <lennoxmucheni@gmail.com>
2023-06-21 03:25:26 +0300

    Print the alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,10 @@
  */
 void print_alphabet(void)
 {
-	char alp[26] = ('a', 'b', 'c', ...,'y', 'z');
+	char letter;
 
-	int i;
-	for (i=0; i < sizeof(alp)/sizeof(alp[0]); i++).
-	{
-		putchar(alp[i]);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
-	return (0);
 }
