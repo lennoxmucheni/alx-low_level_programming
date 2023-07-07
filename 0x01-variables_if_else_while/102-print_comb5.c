ad4aaf5321de4dd00096a58cad3d754e2586258e
Lennox Mucheni <lennoxmucheni@gmail.com>
2023-07-08 01:00:26 +0300

    Added header file main.h

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,23 +11,23 @@ int main(void)
 
 	for (firstNumber = 0; firstNumber <= 99; firstNumber++)
 	{
-		for (secondNumber = firstNumber + 1; secondNumber <= 99; secondNumber++)
+		for (secondNumber = firstNumber; secondNumber <= 99; secondNumber++)
 		{
-			putchar((firstNumber / 10) + '0');
-			putchar((firstNumber % 10) + '0');
-			putchar(' ');
-			putchar((secondNumber / 10) + '0');
-			putchar((secondNumber % 10) + '0');
+            putchar((firstNumber / 10) + '0');
+            putchar((firstNumber % 10) + '0');
+            putchar(' ');
+            putchar((secondNumber / 10) + '0');
+            putchar((secondNumber % 10) + '0');
 
-			if (firstNumber != 98 || secondNumber != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+            if (firstNumber != 99 || secondNumber != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
