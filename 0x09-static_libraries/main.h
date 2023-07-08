14e6ba685689b50de1e539f8191b9d77cf5616de
Lennox Mucheni <lennoxmucheni@gmail.com>
2023-07-08 06:00:57 +0300

    Edited _putchar.c

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 /**
  * File: main.h
  * Author: Lennox Mucheni
- * Description: Header file containing prototypes for all functions
+ * Desc: Header file containing prototypes for all functions
  *       used in the 0x09-static libraries directory.
  */
+
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
