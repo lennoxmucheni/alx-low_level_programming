700d44c517dfd6ac9ede43fca5da99df86a99c6b
Lennox Mucheni <lennoxmucheni@gmail.com>
2023-07-08 04:06:18 +0300

    Updated all files

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,29 @@
+/*
+* File: 0-putchar.c
+* Author: Lennox Mucheni
+* Description: 'Print _putchar, followed by a new line'
+* Return: Always 0 (Success)
+*/
+
+#include "main.h"
+
+/**
+* main - Prints "_putchar" followed by a new line.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
