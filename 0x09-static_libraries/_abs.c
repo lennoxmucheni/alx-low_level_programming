60fadb5deac44285777ecb33a48c700a3cb1f7a6
Lennox Mucheni <lennoxmucheni@gmail.com>
2023-07-08 04:41:15 +0300

    Update in progress

---
+++ 0x09-static_libraries/_abs.c
@@ -0,0 +1,20 @@
+/*
+ * File: _abs.c
+ * Author: Lennox Mucheni
+ */
+
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,29 +1,13 @@
-/*
-* File: 0-putchar.c
-* Author: Lennox Mucheni
-* Description: 'Print _putchar, followed by a new line'
-* Return: Always 0 (Success)
-*/
-
-#include "main.h"
+#include <unistd.h>
 
 /**
-* main - Prints "_putchar" followed by a new line.
-*
-* Return: Always 0.
-*/
-
-int main(void)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+return (write(1, &c, 1));
 }
